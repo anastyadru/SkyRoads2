688d932efc2ddac395df22c486ad20182f09a273
Nastia <adruts@mail.ru>
2023-11-12 05:30:03 +0300

    https://github.com/anastyadru/SkyRoads2.git

--- Assets/Scripts/ButtonNext.cs
+++ Assets/Scripts/ButtonNext.cs
@@ -5,6 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class ButtonNext : MonoBehaviour
 {
+    
+    public bool NextGame;
+    private GameObject nextGameMenu;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("Next"))
+        {
+            LoadMenu();
+        }
+    }
+    
     public void LoadMenu()
     {
         SceneManager.LoadScene("Menu");
