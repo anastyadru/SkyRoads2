0384abfa895d087de15569e46ae232a32d5ef423
Nastia <adruts@mail.ru>
2023-11-13 16:15:41 +0300

    https://github.com/anastyadru/SkyRoads2.git

--- Assets/Scripts/Asteroids.cs
+++ Assets/Scripts/Asteroids.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 public class Asteroids : MonoBehaviour
 {
     [SerializeField] private GameObject asteroid;
-    private void Start()
+    private float sessionTime = 0f; // Переменная для отслеживания времени игровой сессии
+    private float initialSpawnInterval = 1f; // Изначальный интервал появления астероидов
+    private float minSpawnInterval = 0.3f; // Минимальный интервал появления астероидов
+    private float timeBetweenDifficultyIncrease = 15f; // Время между увеличением сложности
+    private float lastDifficultyIncreaseTime = 0f; // Время последнего увеличения сложности
+
+    private void Update()
     {
-        InvokeRepeating("AsteroidsSpace", 0, 1f);
+        sessionTime += Time.deltaTime; // Увеличение времени игровой сессии
+
+        if (sessionTime - lastDifficultyIncreaseTime > timeBetweenDifficultyIncrease)
+        {
+            lastDifficultyIncreaseTime = sessionTime;
+            initialSpawnInterval *= 0.9f; // Уменьшение изначального интервала на 10%
+            if (initialSpawnInterval < minSpawnInterval)
+            {
+                initialSpawnInterval = minSpawnInterval; // Установка минимального интервала, если новый интервал меньше
+            }
+        }
+
+        if (Time.time % initialSpawnInterval < Time.deltaTime)
+        {
+            AsteroidsSpace(); // Вызов метода создания астероидов с текущим интервалом
+        }
     }
 
     private void AsteroidsSpace()
