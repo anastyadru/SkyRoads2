bac8a2d0611897d0251104cf50e594fc41ec1b6f
Nastia <adruts@mail.ru>
2023-11-14 14:56:24 +0300

    https://github.com/anastyadru/SkyRoads2.git

---
+++ Assets/Scripts/SoundMainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class SoundMainMenu : MonoBehaviour
+// {
+//    [Header("Components")]
+//    [SerializeField] private AudoiSource audio;
+//    [SerializeField] private Slider slider;
+//    [SerializeField] private Text text;
+    
+//    [Header("Keys")]
+//    [SerializeField] private string saveVolumeKey;
+    
+//    [Header("Tags")]
+    
+    
+//    [Header("Parameters")]
+    
+//}
