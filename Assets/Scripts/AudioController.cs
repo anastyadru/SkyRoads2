03e7b446e03f766cdab3b0eda91b3c4490a4287d
Nastia <adruts@mail.ru>
2023-11-14 20:26:12 +0300

    https://github.com/anastyadru/SkyRoads2.git

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class AudioController : MonoBehaviour
 {
-    public Slider slider;
+    public AudioSource audio_;
     
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("volume")) audio_.volume = 1;
+    }
 
+    private void Update()
+    {
+        audio_.volume = PlayerPrefs.GetFloat("volume");
+    }
 }
