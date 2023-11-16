acd6ee60c9c69c6056371d427f00e5d65d5c2124
Nastia <adruts@mail.ru>
2023-11-16 15:53:10 +0300

    https://github.com/anastyadru/SkyRoads2.git

---
+++ Assets/Scripts/ToggleController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ToggleController : MonoBehaviour
+{
+
+    public void ToggleMusic(bool enabled)
+    {
+        
+    }
+}
