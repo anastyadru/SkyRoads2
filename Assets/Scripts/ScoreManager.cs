a592f16a99ec400f81ad41715e07a3fba5c69db1
Nastia <adruts@mail.ru>
2023-11-16 04:25:27 +0300

    https://github.com/anastyadru/SkyRoads2.git

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private Text HighScoreText;
     [SerializeField] private Text ScoreText;
 
-    public float score;
+    public int score;
     public int highscore;
 
     void Start()
